e88ddd5924d552e455dae78b6bd28d6b23ad1079
Sam Peterson <speterson@reachmedianetwork.com>
2023-11-30 23:21:15 +0100

    Add: part 2 of day 1.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { day1Part1, day1Part1ExampleData } from "./2023";
+import { day1ExampleData, day1Part1, day1Part2 } from "./2023";
 
 export * from "./2023";
 
 // Run tests here?
 
-console.log(day1Part1(day1Part1ExampleData));
+console.log(day1Part1(day1ExampleData).sum);
+
+console.log(day1Part2(day1ExampleData).sum);
