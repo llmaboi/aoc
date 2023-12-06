2434b2ec8fb397e9017bbfc079c311836e2e9fd5
Sam Peterson <samllama55@gmail.com>
2023-12-06 10:33:55 +0100

    CHANGE: Generator test file. ADD: Day6 part 1 and 2.

--- src/day.test.ts
+++ src/day.test.ts
@@ -8,11 +8,25 @@ describe('Day #', () => {
 
       expect(true).toBeTrue();
     });
+
+    test('Actual Test', async () => {
+      const testDataFile = Bun.file(`${import.meta.dir}/day#.example.data.txt`);
+      const testData = await testDataFile.text();
+
+      expect(true).toBeTrue();
+    });
   });
 
   describe('Part 2', () => {
     test('Example Test', async () => {
-      const testDataFile = Bun.file(`${import.meta.dir}/day#.data.txt`);
+      const testDataFile = Bun.file(`${import.meta.dir}/day#.example.data.txt`);
+      const testData = await testDataFile.text();
+
+      expect(true).toBeTrue();
+    });
+
+    test('Actual Test', async () => {
+      const testDataFile = Bun.file(`${import.meta.dir}/day#.example.data.txt`);
       const testData = await testDataFile.text();
 
       expect(true).toBeTrue();
